32de44b81379e3a6455fa749d0a6e44a00b27c07
Bryan Bicera <brheanbr@gmail.com>
2020-09-01 09:53:10 +0800

    ADDED DATA CONVERTION SERVICE

---
+++ LoginApp/Entity/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginApp.Entity
+{
+    public class Person
+    {
+        public string DistinguishedName { get; set; }
+        public string Name { get; set; }
+        public bool Enabled { get; set; }
+        public string SamAccountName { get; set; }
+    }
+}
---
+++ LoginApp/Services/DataConvertion.cs
@@ -0,0 +1,29 @@
+﻿using LoginApp.Entity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginApp.Services
+{
+    public class DataConvertion
+    {
+
+        public string ConvertJsonToClass()
+        {
+            string json = "{\"DistinguishedName\":\"CN=Jacob Benner,OU=SlaitUsers,OU=Enterprise,DC=cloud,DC=mcc,DC=loc\",\"Name\":\"Jacob Benner\",\"Enabled\":true,\"SamAccountName\":\"jacob.benner\"}," +
+                    "{\"DistinguishedName\":\"CN=Jacob Benner,OU=SlaitUsers,OU=Enterprise,DC=cloud,DC=mcc,DC=loc\",\"Name\":\"A a\",\"Enabled\":false,\"SamAccountName\":\"A.a\"}";
+            Person data = JsonConvert.DeserializeObject<Person>(json);
+            Console.WriteLine("DistinguishedName: " + data.DistinguishedName);
+            Console.WriteLine("Name: " + data.Name);
+            Console.WriteLine("Enabled: " + data.Enabled);
+            Console.WriteLine("SamAccountName: " + data.SamAccountName);
+            return data.DistinguishedName;
+        }
+    }
+
+   
+
+
+}
